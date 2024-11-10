af6f82a21a1c6552e8b663e3052090b35db50208
Muhammad Talha Saeed <iamtalha971@gmail.com>
2024-11-10 13:11:41 +0400

    feat: Heri Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,11 @@ export default function Home() {
   console.log('Hey What I am?')
   return (
     <>
-  <h1 className="text-3xl">Welcome Home Page</h1>
+    <section className="pink_container">
+  <h1 className="heading">Pitch Your Starup, <br /> Connect with enterpreneurs Home Page</h1>
+  <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+  Competitions.</p>
+    </section>
     </>
   );
 }
