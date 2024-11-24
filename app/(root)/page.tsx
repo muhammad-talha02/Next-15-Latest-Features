9aed2b5c1e172bd4e8c5598bdff68eb319399c05
Muhammad Talha Saeed <iamtalha971@gmail.com>
2024-11-24 09:27:16 +0400

    feat -> search filter added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,23 +9,11 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams)?.query;
-// const posts = await client.fetch(STARTUPS_QUERY) as StartupCardType[]
-const {data:posts} = await sanityFetch({query:STARTUPS_QUERY})
+
+const params ={ search:query || null}
+  const {data:posts} = await sanityFetch({query:STARTUPS_QUERY, params})
 
 console.log(JSON.stringify(posts, null, 2))
-  // const posts = [
-  //   {
-  //     _id: 1,
-  //     _createdAt: new Date(),
-  //     views: 55,
-  //     _author: { _id: 5, name: "Talha" },
-  //     description: "This is a description",
-  //     image:
-  //       "https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //     category: "Robots",
-  //     title: "We Robots",
-  //   },
-  // ];
 
   return (
     <>
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY = defineQuery(`
-    *[_type == 'startup' && defined(slug.current)] | order(_createdAt desc){
+    *[_type == 'startup' && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc){
   _id,
     title,
     slug,
@@ -13,4 +13,4 @@ _id , name, image, bio
     
     views,
     description, category, image
-}`)
+}`);
